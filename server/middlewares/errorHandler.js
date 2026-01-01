39bd11fc9a0a405d6ee38ea92995ca860aeaf37a
Aldy Rian <naiksatulevel.idn@gmail.com>
2026-01-02 01:52:28 +0700

    update file upload

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -1,4 +1,12 @@
 const errorHandler = (err, req, res, next) => {
+  // Handle Multer Errors
+  if (err.name === 'MulterError') {
+    if (err.code === 'LIMIT_FILE_SIZE') {
+        return res.status(400).json({ message: 'File too large. Maximum size is 1MB.' });
+    }
+    return res.status(400).json({ message: err.message });
+  }
+
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode);
   res.json({
